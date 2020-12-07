0e29cc1ad8a8918c86cb93840eb67483101a01cd
Clay Hoover <ClayHoover@Clays-MBP.attlocal.net>
2020-12-07 13:11:27 -0500

    12/7 AM

---
+++ server/controllers/notesController.js
@@ -0,0 +1,19 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+      db.Book
+        .find(req.query)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        console.log("create function in controller");
+        db.Book
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
+
+module.exports
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const notes = require("./notes");
 
 // Book routes
 router.use("/books", bookRoutes);
+router.use("/notes", notes);
 
 module.exports = router;
